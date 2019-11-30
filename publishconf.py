ecb1a93ba90b6940e6b84aed819aaa4239a96f54
Aravinda VK <mail@aravindavk.in>
2019-11-30 13:54:12 +0530

    Updated site URL
    Signed-off-by: Aravinda VK <mail@aravindavk.in>

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 
-SITEURL = "http://aravindavk.in"
+SITEURL = "https://aravindavk.in"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
